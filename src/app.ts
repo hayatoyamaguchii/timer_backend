ae356aae95d37de27cd63cd0d7e190ce3d1af395
hayatoyamaguchii <yamaguchihayatoo@gmail.com>
2024-11-30 08:51:08 +0900

    [feature]登録、ログイン、ログアウト機能の実装。

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import registerRoutes from "./routes/registerRoutes";
+import loginRoutes from "./routes/loginRoutes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("/api/register", registerRoutes);
+app.use("/api/login", loginRoutes);
 
 export default app;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,23 +1,10 @@
-import { Router } from "express";
+import express from "express";
 import promisePool from "../config/db";
 
-const router = Router();
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("GET request to the homepage");
-});
-
-router.post("/", (req, res) => {
-    res.send("POST request to the homepage");
-});
-
-router.get("/alldatabase", async (req, res) => {
-    try {
-        const [rows] = await promisePool.execute("SELECT * FROM users");
-        res.json(rows);
-    } catch (error) {
-        res.status(500).json({ error: "Database error" });
-    }
+    res.send("GET request to users");
 });
 
 export default router;
